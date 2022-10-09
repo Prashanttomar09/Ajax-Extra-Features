b8657f79c7179baf2a256540807b500433029262
Prashanttomar09 <114228664+Prashanttomar09@users.noreply.github.com>
2022-10-09 09:31:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('𝙿𝚁𝙸𝚅𝙰𝚃𝙴 𝙱𝙾𝚃', url=f"https://t.me/AboutAadhi")
+                InlineKeyboardButton('𝙿𝚁𝙸𝚅𝙰𝚃𝙴 𝙱𝙾𝚃', url=f"https://t.me/+wD0CAlK5bDMzZmM1")
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
