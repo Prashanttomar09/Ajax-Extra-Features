8dcb10f8a36e7ef8bde0d76b7a6f0a985ae33557
Prashanttomar09 <114228664+Prashanttomar09@users.noreply.github.com>
2022-10-09 08:08:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Prashanttomar09/Ajax-Extra-Features /Ajax-Extra-Features
+  git clone https://github.com/Prashanttomar09/Ajax-Extra-Features /Ajax
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax-Extra-Features
+  git clone $UPSTREAM_REPO /Ajax
 fi
-cd /Ajax-Extra-Features
+cd /Ajax
 pip3 install -U -r requirements.txt
 echo "Starting Killer....🔥"
 python3 bot.py
