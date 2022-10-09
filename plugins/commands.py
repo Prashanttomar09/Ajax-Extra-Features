8f1442a7b5e5a3bc7aa29a618031b9e93ca0b59c
Prashanttomar09 <114228664+Prashanttomar09@users.noreply.github.com>
2022-10-09 09:28:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -235,7 +235,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/OpusTechz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://t.me/filmyduniya_backup') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
